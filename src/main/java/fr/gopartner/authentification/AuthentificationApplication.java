fb51e8f19c3e37d866ccfda0b7561569b8f8f894
wajih boufaied <wboufaied@gopartner.fr>
2023-05-12 15:45:06 +0100

    Creation of authentication project skeleton

---
+++ src/main/java/fr/gopartner/authentification/AuthentificationApplication.java
@@ -0,0 +1,13 @@
+package fr.gopartner.authentification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthentificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthentificationApplication.class, args);
+    }
+
+}
